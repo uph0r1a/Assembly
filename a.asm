d0b9e14d81aa8da878bbbd10a8e5acd5ce9f48b7
uph0r1a <tdtri1712@gmail.com>
2025-12-09 20:39:15 +0700

    a

--- a.asm
+++ a.asm
@@ -1,16 +1,51 @@
 section .data
-    hello db 'Hello, World!',0
+    arr     dd 1, 3, 1, 1, 4    
+    goal    dd 1                
+    msg     db "0", 10          
 
 section .text
-    global main
+    global _start               
 
-main:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, 13
-    int 0x80
+_start:
 
-    mov eax, 1
-    int 0x80
-    
+    xor rcx, rcx                
+    xor rbx, rbx               
+
+loop_start:
+    cmp rcx, 5                  
+    jge loop_end               
+
+    mov eax, [arr + rcx*4]     
+                               
+    cmp eax, [goal]             
+    jne skip_inc                
+
+    inc rbx                     
+
+skip_inc:
+    inc rcx                    
+    jmp loop_start              
+
+loop_end:
+    mov al, bl                  
+    add al, '0'                 
+    mov [msg], al               
+
+    mov rax, 1                  
+    mov rdi, 1                 
+    mov rsi, msg              
+    mov rdx, 2                 
+    syscall                    
+
+    mov rax, 60                
+    xor rdi, rdi              
+    syscall                    
+
+
+
+
+
+
+
+
+
